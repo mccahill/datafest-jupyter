d4057957f4b266d45f6fdd66019a72b6e08554df
Mark McCahill <mark.mccahill@duke.edu>
2017-08-13 16:00:17 -0400

    disable_check_xsrf check
    Signed-off-by: Mark McCahill <mark.mccahill@duke.edu>

--- src/BIO821-notebook/jupyter_notebook_config.py
+++ src/BIO821-notebook/jupyter_notebook_config.py
@@ -11,9 +11,8 @@ c.NotebookApp.port = int(os.getenv('PORT', '') or 8888)
 c.NotebookApp.open_browser = False
 
 # disable XSRF if requested
-#if 'DISABLE_XSRF' in os.environ:
-c.NotebookApp.disable_check_xsrf = True
-c.NotebookApp.token = passwd(os.environ['PASSWORD'])
+if 'DISABLE_XSRF' in os.environ:
+  c.NotebookApp.disable_check_xsrf = True
 
 # Set a certificate if USE_HTTPS is set to any value
 if 'USE_HTTPS' in os.environ:
